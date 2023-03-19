703eeea4d7ba3e9949fe16c4cc6743295a28a495
Rodolpho <redsmedeiros@gmail.com>
2023-03-19 19:51:54 -0300

    servidor

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,17 @@
-console.log('teste');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//rotas
+app.use(routes);
+
+const PORT = 3333;
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor na porta ${PORT}`);
+})
